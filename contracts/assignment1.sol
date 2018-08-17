c27568b96d3100f8c7aa99d77c50aff596e361bf
Al Lakhani <waaghree@hotmail.com>
2018-08-16 18:26:54 -0700

    New code added

--- contracts/assignment1.sol
+++ contracts/assignment1.sol
@@ -44,3 +44,67 @@ contract trial {
         //return usersInLevel;
   }
 }
+/*pragma solidity ^0.4.19;
+
+
+
+contract abc {
+  
+ 
+  
+  struct User {
+      bytes32 name;
+      uint score;
+      
+  }
+  
+      
+  
+  
+  User[] public userArray;
+  
+  //mapping(uint => User) userAccess;
+  
+  
+  function setuser(string _name, uint _score) public {
+      User memory newUser;   
+      newUser.name = stringToBytes32(_name);
+      newUser.score = _score;
+      
+      
+      //userAccess[level].push(newUser);
+      userArray.push(newUser);
+      
+  }
+  
+  function stringToBytes32(string memory source) public pure returns (bytes32 result) {
+    bytes memory tempEmptyStringTest = bytes(source);
+    if (tempEmptyStringTest.length == 0) {
+        return 0x0;
+    }
+
+    assembly {
+        result := mload(add(source, 32))
+    }
+}
+  
+  function getUser() view public returns(bytes32[], uint[]){
+      //return(userAccess[level]);
+      uint length = userArray.length;
+      
+      bytes32[] memory nameName = new bytes32[](length);
+      uint[] memory scoreScore = new uint[](length);
+      
+      for (uint i = 0; i < userArray.length; i++){
+      nameName[i] = userArray[i].name;
+      scoreScore[i] = userArray[i].score;
+
+    }
+  return (nameName, scoreScore);
+  }
+     
+}
+//User memory currentUser;
+      //currentUser = userArray[i];
+      //nameName[i] = currentUser.name;
+      //scoreScore[i] = currentUser.score;  */
